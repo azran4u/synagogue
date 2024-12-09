b5726916835adf4963045030206494e37416ab56
Eyal Azran <azran4u@gmail.com>
2024-12-09 17:29:23 +0200

    arrange test

--- functions/admin/test_main.py
+++ functions/admin/test_main.py
@@ -1,20 +1,20 @@
-from main import controller
-from werkzeug.test import EnvironBuilder
-from werkzeug.wrappers import Request
-
 # test_main.py
-# from upload_images import upload_images
+
 # from sync import sync
+# sync()
+
 # from export_orders import export_orders
-
-# sync()
-# sync()
 # export()
-# upload_images()
 
-# Create a mock request
-builder = EnvironBuilder(method="GET", path="/sync")
-env = builder.get_environ()
-mock_request = Request(env)
-response = controller(mock_request)
-print(response.get_data(as_text=True))
+from upload_images import upload_images
+
+upload_images()
+
+# from main import controller
+# from werkzeug.test import EnvironBuilder
+# from werkzeug.wrappers import Request
+# builder = EnvironBuilder(method="GET", path="/sync")
+# env = builder.get_environ()
+# mock_request = Request(env)
+# response = controller(mock_request)
+# print(response.get_data(as_text=True))
