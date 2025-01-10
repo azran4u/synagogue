dc38b2b8457d3c590f32e7b2115f18eaa520193c
Eyal Azran <eyal.azran@cyberark.com>
2025-01-10 10:55:15 +0200

    change images path

--- functions/admin/upload_images.py
+++ functions/admin/upload_images.py
@@ -8,8 +8,8 @@ from utils.filesystem_utils import delete_local_folder_and_content
 
 
 def upload_images():
-    images_path_input = "/Users/eyalazran/Downloads/app_images/סופי"
-    images_path_output = "/Users/eyalazran/Downloads/app_images/סופי/upload"
+    images_path_input = "~/Downloads/tights-shomron-images"
+    images_path_output = images_path_input + "uploaded"
     delete_local_folder_and_content(images_path_output)
     downscale_images(images_path_input, images_path_output)
     bucket_name = "shomron-tights-images"
