99715eabfea7a7cd6efaf2f1ecb8c0ce8c7eae78
Eyal Azran <azran4u@gmail.com>
2025-02-04 23:17:08 +0200

    add short description to product sync

--- functions/admin/export_orders.py
+++ functions/admin/export_orders.py
@@ -34,18 +34,14 @@ def allowed_admins():
 
 
 def export_orders():
-    collection_name = "orders"
-    orders = read_firestore_collection(collection_name)
-    # current_sale_name = current_sale()
-    # current_sale_orders = [
-    #     order for order in orders if order.get("saleName") == current_sale_name
-    # ]
-
-    current_sale_orders = orders
+    orders = read_firestore_collection("orders")
+    products = read_firestore_collection("products")
+    products_df = pd.DataFrame(products)
 
     orders_list = []
-    products_list = []
-    for order in current_sale_orders:
+    packeging_list = []
+
+    for order in orders:
         order_id = order.get("id")
         order_date = order.get("date")
         order_comments = order.get("comments")
@@ -57,54 +53,54 @@ def export_orders():
         order_sale_name = order.get("saleName")
         order_total_price = order.get("totalCost")
         order_total_cost_after_discount = order.get("totalCostAfterDiscount")
-        products = order.get("products", [])
-        total_amount = sum(product.get("amount", 0) for product in products)
+        order_cart_products = order.get("products", [])
+        total_amount = sum(product.get("amount", 0) for product in order_cart_products)
 
         order_entry = {
             "תאריך": order_date,
             "נקודת חלוקה": order_preffered_pickup_location,
             "שם פרטי": order_first_name,
             "שם משפחה": order_last_name,
-            "מחיר לפני הנחה": order_total_price,
             "מחיר לאחר הנחה": order_total_cost_after_discount,
             "כמות פריטים": total_amount,
             "טלפון נייד": order_phone_number,
             "הערות": order_comments,
+            "האם נארז": "",
+            "אמצעי תשלום": "",
+            "למי שולם": "",
             "כתובת מייל": order_email,
             "קישור להזמנה": f"{base_frontend_url}/order/{order_id}",
             "שם מכירה": order_sale_name,
+            "מחיר לפני הנחה": order_total_price,
         }
         orders_list.append(order_entry)
 
-        for row in products:
-            amount = row.get("amount")
-            product = row.get("product")
+        for order_cart_product in order_cart_products:
+            amount = order_cart_product.get("amount")
+            product = order_cart_product.get("product")
+
+            id = product.get("id")
             supplier = product.get("supplier")
-            if product.get("kind") == "tights":
-                description = f"טייץ גרביון, {product.get('denier')} דניר, {product.get('leg')} רגל, מידה {product.get('size')}, צבע {product.get('color')}"
-            elif product.get("kind") == "lace":
-                description = (
-                    f"טייץ תחרה, תחרה {product.get('lace')} , מידה {product.get('size')}, צבע {product.get('color')}"
-                )
-            elif product.get("kind") == "short":
-                description = f"טייץ קצר, אורך {product.get('length')}, מידה {product.get('size')} ,צבע {product.get('color')}"
-            elif product.get("kind") == "thermal":
-                description = f"טייץ תרמי, {product.get('leg')} רגל, מידה {product.get('size')} , צבע {product.get('color')} "
-            product_entry = {
+
+            product_from_products = products_df[products_df["id"] == id].iloc[0]
+            short_description = product_from_products.get("short_description")
+            
+            pageging_entry = {
                 "שם פרטי": order_first_name,
                 "שם משפחה": order_last_name,
                 "טלפון נייד": order_phone_number,
                 "נקודת חלוקה": order_preffered_pickup_location,
-                "המוצר": description.strip(),
+                "המוצר": short_description,
                 "כמות": amount,
                 "כמות פריטים בהזמנה": total_amount,
-                "הערות": order_comments,
+                "מחיר לאחר הנחה": order_total_cost_after_discount,
+                "ארוז": "",
                 "ספק": supplier,
             }
-            products_list.append(product_entry)
+            packeging_list.append(pageging_entry)
 
     orders_df = pd.DataFrame(orders_list)
-    products_df = pd.DataFrame(products_list)
+    pageging_df = pd.DataFrame(packeging_list)
 
     orders_groupby_pickup_df = (
         orders_df.groupby("נקודת חלוקה")
@@ -112,26 +108,35 @@ def export_orders():
         .reset_index()
     )
     orders_groupby_pickup_df["עמלה"] = orders_groupby_pickup_df["מחיר לאחר הנחה"] * 0.10
+    
     orders_count = (
         orders_df.groupby("נקודת חלוקה").size().reset_index(name="מספר הזמנות")
     )
+    
     orders_groupby_pickup_df = pd.merge(
         orders_groupby_pickup_df, orders_count, on="נקודת חלוקה"
     )
+    orders_groupby_pickup_df["למי לשלם"] = ""
 
     suppliers_df = (
-        products_df.groupby(["ספק", "המוצר"]).agg({"כמות": "sum"}).reset_index()
+        pageging_df.groupby(["ספק", "המוצר"]).agg({"כמות": "sum"}).reset_index()
     )
 
+    suppliers_with_products_df = pd.merge(suppliers_df, products_df, how="left", left_on=["המוצר"], right_on=["short_description"])
+
+    suppliers_df = suppliers_with_products_df[["ספק", "המוצר", "כמות", "stock_liron", "stock_sharale", "units_in_package"]]
+    
+    suppliers_df = suppliers_df.rename(columns={"stock_liron": "מלאי לירון", "stock_sharale": "מלאי שהרלה", "units_in_package": "יחידות באריזה"})
+    
     sheet_title = "shomron-tights" + "@" + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     gmail_accounts = allowed_admins()
 
     # Dictionary of tab names and DataFrames
     tabs_data = {
-        "הזמנות": orders_df,
-        "אריזות": products_df,
-        "מכירות לפי ישוב": orders_groupby_pickup_df,
-        "ספקים": suppliers_df,
+        "הזמנות": orders_df.sort_values(by=["נקודת חלוקה", "שם משפחה", "שם פרטי"], ascending=[True, True, True]),
+        "אריזות": pageging_df.sort_values(by=["נקודת חלוקה", "שם משפחה", "שם פרטי", "המוצר"], ascending=[True, True, True, True]),
+        "מכירות לפי ישוב": orders_groupby_pickup_df.sort_values(by=["נקודת חלוקה"], ascending=[True]),
+        "ספקים": suppliers_df.sort_values(by=["ספק", "המוצר"], ascending=[True, True]),
     }
 
     # Create the Google Sheet and set permissions
@@ -139,3 +144,5 @@ def export_orders():
         sheet_title, gmail_accounts, tabs_data
     )
     return spreadsheet.url
+
+export_orders()
--- functions/admin/sync.py
+++ functions/admin/sync.py
@@ -46,8 +46,53 @@ def save_dfs_to_firestore(dataframes):
                 + df[columns_to_concatenate].astype(str).agg("_".join, axis=1)
             )            
             collection_name = tab_name
+
+            if tab_name == "products":
+                df["short_description"] = df.apply(product_short_description, axis=1)
+
             write_df_to_firestore(df, collection_name, "id")
         else:
             print(f"Skipping tab: {tab_name}")
+
         print(f"DataFrame for tab: {tab_name} saved successfully")
     print("DataFrames saved to Firestore successfully")
+
+def product_short_description(df):
+    kind = df.get("kind")
+    name = df.get("name")
+    size = df.get("size")
+    color = df.get("color")
+    length = df.get("length")
+    lace = df.get("lace")
+    leg = df.get("leg")
+    denier = df.get("denier")
+
+
+    women_tights_name = "טייץ גרביון לנשים ונערות"
+    girls_tights_name = "טייץ גרביון ילדות"
+    girls_hebrew = "ילדות"
+    lace_hebrew = "תחרה"
+    tights_hebrew = "טייץ"
+    thermal_hebrew = "טרמי"
+    denier_hebrew = "דניר"
+    with_leg_hebrew = "עם רגל"
+    without_leg_hebrew = "ללא רגל"
+    if leg == "leg":
+        leg_label = with_leg_hebrew
+    else:
+        leg_label = without_leg_hebrew
+
+    if kind == "tights" and name == women_tights_name:
+        description = f"{denier} {denier_hebrew} {size} {leg_label} {color}"
+    elif kind == "tights" and name == girls_tights_name:
+        description = f"{girls_hebrew} {size} {leg_label} {color}"
+    elif kind == "lace":
+        description =f"{lace_hebrew} {lace} {size} {color}"                
+    elif kind == "short":
+        description = f"{tights_hebrew} {length} {color}"
+    elif kind == "thermal":
+        description = f"{thermal_hebrew} {size} {leg_label} {color}"
+    else:
+        print (f"ERROR - no description for kind {kind} and name {name}")
+        exit(1)
+    return description
